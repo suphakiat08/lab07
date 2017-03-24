0f0b41a62f117444dddc9d28f6a54a8face0fe63
suphakiat08 <suphakiat08@gmail.com>
2017-03-24 11:27:52 +0700

    Update dopost.php

--- dopost.php
+++ dopost.php
@@ -4,12 +4,12 @@
         if($_POST['sex'] == "M") { $sex="ชาย"; } 
         else { $sex="หญิง"; }
 
-        if($_POST['intrL'] == "L" && $_POST['intrG'] == "") { $favorite="เรียน"; } 
-        else if($_POST['intrL'] == "" && $_POST['intrG'] == "G") { $favorite="เกมส์"; }
-        else if($_POST['intrL'] == "L" && $_POST['intrG'] == "G"){ $favorite="เรียน<br>เกมส์"; }
+        if($_POST['intrL'] == "L" && $_POST['intrG'] == "") { $favorite1="เรียน"; } 
+        else if($_POST['intrL'] == "" && $_POST['intrG'] == "G") { $favorite1="เกมส์"; }
+        else if($_POST['intrL'] == "L" && $_POST['intrG'] == "G"){ $favorite1="เรียน";$favorite2="เกมส์"; }
 
         $sql = "INSERT INTO Lab07(name,email,sex,favorite,address,province_id) VALUES('".$_POST['name']."','".$_POST['email'].
-            "','".$sex."','".$favorite."','".$_POST['address']."',".$_POST['province'].")";
+            "','".$sex."','".$favorite1."','".$favorite2."','".$_POST['address']."',".$_POST['province'].")";
         $conn->query($sql);
     }
 
